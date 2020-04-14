64b708d92b64206df638cf964d6d4685974d71f9
shoya.ht <taro.kirameki@techacademy.jp>
2020-04-14 23:07:20 +0900

    Add database files

---
+++ src/models/Message.java
@@ -0,0 +1,70 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="messages")
+public class Message {
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="titile",length=225,nullable=false)
+    private String title;
+
+    @Column(name="content",length=225,nullable=false)
+    private String content;
+
+    @Column(name="created_at",nullable=false)
+    private String created_at;
+
+    @Column(name="updated_at",nullable=false)
+    private String updated_at;
+
+    public Integer getId(){
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+}
